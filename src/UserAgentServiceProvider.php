59a4162cda7b7635ce26ef755d38b0af8d9e46c7
Nikita <nnn.mail44@gmail.com>
2022-11-07 00:16:20 +0200

    init

---
+++ src/UserAgentServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Nick44\UserAgent;
+
+use Illuminate\Support\ServiceProvider;
+
+class UserAgentServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->app->singleton(UserAgentClientServiceInterface::class, function(){
+            return new PhpUserAgentService();
+        });
+    }
+
+}
